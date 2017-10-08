3032f827428a39ede812968a6f1ade6e4c213088
christinebuechler <32618013+christinebuechler@users.noreply.github.com>
2017-10-08 18:37:37 +0200

    Create mdwind.py
    Modul zum Finden der realen Windgeschwindigkeit. Da wir die voraussichtlich nicht brauchen, wandeln wir sie um, um den realen Windwinkel zu erhalten.

---
+++ mdwind.py
@@ -0,0 +1,9 @@
+# windrichtung und geschwindigkeit
+import math
+
+
+# funktion gibt wahre Windgeschwindigkeit zurueck
+def RealWindSpeed(ApparentWind, BoatSpeed, ApparentAngle):
+ result = -99
+ result = math.sqrt(ApparentWind**2 + BoatSpeed**2 - 2*ApparentWind*BoatSpeed*math.cos(ApparentAngle))
+ return result      
