8f40145baaef34595d3cdf70d23bdd0b543aae7e
svensutter <32609779+svensutter@users.noreply.github.com>
2017-10-08 18:39:42 +0200

    Update mdwind.py

--- mdwind.py
+++ mdwind.py
@@ -7,3 +7,4 @@ def RealWindSpeed(ApparentWind, BoatSpeed, ApparentAngle):
  result = -99
  result = math.sqrt(ApparentWind**2 + BoatSpeed**2 - 2*ApparentWind*BoatSpeed*math.cos(ApparentAngle))
  return result      
+test
