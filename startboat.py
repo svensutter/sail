8683ec3f8d52721f88e51ac5d5a6967c87c4f018
christinebuechler <32618013+christinebuechler@users.noreply.github.com>
2017-10-13 21:39:01 +0200

    Add files via upload

---
+++ startboat.py
@@ -0,0 +1,7 @@
+# Skript, das aus Bootvorgang gestartet wird, von dem geht alles aus
+# Standardsgeschwindigkeitsformat in kmh
+
+
+# Versuch, wird nacher wieder geloescht
+import mdwind
+print mdwind.RealWindAngle(30, 5, 30)
