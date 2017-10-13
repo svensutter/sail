b466d5ad31a49a68f9643bb3fcccac69dfe81756
christinebuechler <32618013+christinebuechler@users.noreply.github.com>
2017-10-13 21:37:52 +0200

    Add files via upload

--- mdwind.py
+++ mdwind.py
@@ -2,9 +2,16 @@
 import math
 
 
-# funktion gibt wahre Windgeschwindigkeit zurueck
-def RealWindSpeed(ApparentWind, BoatSpeed, ApparentAngle):
- result = -99
- result = math.sqrt(ApparentWind**2 + BoatSpeed**2 - 2*ApparentWind*BoatSpeed*math.cos(ApparentAngle))
- return result      
-test
+# funktion gibt wahrer Windwinkel zurueck
+def RealWindAngle(ApparentWind, BoatSpeed, ApparentAngle):
+ RealWindSpeed = math.sqrt(ApparentWind**2 + BoatSpeed**2 - 2*ApparentWind*BoatSpeed*math.cos(ApparentAngle))
+ 
+ result_radiant = math.acos((ApparentWind*math.cos(ApparentAngle)-BoatSpeed)/RealWindSpeed)
+ result = math.degrees(result_radiant)
+ if (ApparentAngle > 180 and ApparentAngle < 360) or (ApparentAngle < 0 and ApparentAngle > -180):
+  result = -result
+  
+ if result < 0:
+  result = result + 360
+ 
+ return result  
