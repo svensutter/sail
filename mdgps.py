4e7d4a45484be20c77f26d7e9ca0829ff7adcbbb
svensutter <32609779+svensutter@users.noreply.github.com>
2017-10-22 13:36:38 +0200

    Update mdgps.py

--- mdgps.py
+++ mdgps.py
@@ -1,1 +1,5 @@
 # Hier finden alle Funktionen zu GPS Platz. Dazu gehören Positionsbestimmung, Kompass, Winkel zum Ziel.
+
+
+# IDEE: Objekt erstellen für 3-Layer-Kompass erstellen, dass dann verschiedene Abfragen zulaesst
+#       wie bspw. in welchem Windbereich der Zielpunkt liegt.
