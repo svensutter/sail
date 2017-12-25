28a1f7ebc02fb6b2c16eb05ec4b254f5740ebd40
svensutter <32609779+svensutter@users.noreply.github.com>
2017-12-25 15:56:40 +0100

    Mailabruf mit Speicherung des Contents stable

--- mdmail.py
+++ mdmail.py
@@ -10,14 +10,33 @@ Mail_Smtp = "smtp.gmail.com"
 
 import imaplib
 
-# Funktion GetURLFromMail, ruft neuste E-Mail ab, extrahiert Shortlink
-# und gibt diesen zurueck
-def GetURLFromMail():
+# Funktion GetTargetFromMail, ruft neuste E-Mail ab, extrahiert Shortlink
+# und bestimmt die Position daraus (via Internet) und gibt Koordinaten
+# als Liste zurueck, oder "none", wenn keine neue Mail eingetroffen ist
+def GetTargetFromMail():
  im = imaplib.IMAP4_SSL(Mail_Imap, Mail_ImapPort) # Verbindung aufbauen
+ # falls gmail verwendet wird, muss dort Zugriff von unsicheren Apps
+ # erlaubt werden, sonst muesste xauth2 implementiert werden
  im.login(Mail_User, Mail_Paswd) ### LOGIN
  
- print(im.list())
+ im.select("INBOX")
+ mailids = im.search(None, '(SUBJECT "Gesetzte Markierung")') # IDs liegen an zweiter Listenstelle, wiederum als Liste
  
+ for id in mailids[1][0].split(): # Maximale ID herausfinden
+  mailid_max = id
+  
+ mail_content = im.fetch(mailid_max, "(BODY[TEXT])")
+ print(mail_content)
+ 
+ for id in mailids[1][0].split(): # Alle Mails mit den besagten IDs loeschen
+   im.store(id, '+FLAGS', '\\Deleted')
+ im.expunge()
+
  im.logout() ### LOGOUT
  
-print(GetURLFromMail()) # test
+ # Was noch fehlt:
+ # 1) "none"-Rueckgabe, falls keine neue Mail
+ # 2) Link aus mail_content auslesen
+ # 3) Linkuebergabe an GetGooglePosition(url), die Positionsdaten ermittelt, mit return
+ 
+print(GetTargetFromMail()) # test
