acfb61c38c71277972d047230466cf01f5b30260
svensutter <32609779+svensutter@users.noreply.github.com>
2017-12-25 10:08:48 +0100

    Update mdmail.py

--- mdmail.py
+++ mdmail.py
@@ -1,5 +1,13 @@
- Modul mdmail, enthaelt Funktionen zum Kontakt mit Mailserver und Extraktion
+# Modul mdmail, enthaelt Funktionen zum Kontakt mit Mailserver und Extraktion
 # der Inhalte.
 
 # CONFIG
-Mail_User = "captain.proboat
+Mail_User = "captain.proboat@gmail.com"
+Mail_Paswd = "ub6-NdW-dv2-z3e"
+Mail_Imap = "imap.gmail.com"
+Mail_Smtp = "smtp.gmail.com"
+
+# Funktion GetURLFromMail, ruft neuste E-Mail ab, extrahiert Shortlink
+# und gibt diesen zurueck
+def GetURLFromMail(url):
+ # ..... code ....
