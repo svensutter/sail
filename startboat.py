8c1cb83d59f411e494ed38ac5566097589d108cd
svensutter <32609779+svensutter@users.noreply.github.com>
2017-12-27 14:23:19 +0100

    Module hinzugefügt

--- startboat.py
+++ startboat.py
@@ -1,4 +1,8 @@
 # Skript, das aus Bootvorgang gestartet wird, von dem geht alles aus
 # Standardsgeschwindigkeitsformat in kmh
 
+import mdmail
+import mdgps
 
+# TEST TEST TEST
+print(GetTargetFromMail())
