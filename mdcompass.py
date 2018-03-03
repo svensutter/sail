d7d83390f877542cb10d7a730d6dd668b1d4aec9
svensutter <32609779+svensutter@users.noreply.github.com>
2018-03-03 11:35:13 +0100

    Update mdcompass.py

--- mdcompass.py
+++ mdcompass.py
@@ -7,9 +7,8 @@ import mdhmc5883l
 # Wir erhalten eine Zahl zwischen 0 und 360.
 def BoatToNorth():
     # Read Compass-Accelerometer raw value
-    X_axis_H = 0x03  # Address of X-axis MSB data register (HMC5883l)
-    Z_axis_H = 0x05  # Address of Z-axis MSB data register (HMC5883l)
-    Y_axis_H = 0x07  # Address of Y-axis MSB data register (HMC5883l)
-    x = mdhmc588l.read_raw_data(X_axis_H)
-    z = mdhmc588l.read_raw_data(Z_axis_H)
-    y = mdhmc588l.read_raw_data(Y_axis_H)
+    x = mdhmc588l.read_raw_data(0x03)
+    z = mdhmc588l.read_raw_data(0x05)
+    y = mdhmc588l.read_raw_data(0x07)
+    
+    
