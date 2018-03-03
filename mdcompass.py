095027693e4f5e828871deab4867c29e19dc9618
svensutter <32609779+svensutter@users.noreply.github.com>
2018-03-03 12:52:31 +0100

    Update mdcompass.py

--- mdcompass.py
+++ mdcompass.py
@@ -1,6 +1,7 @@
 # Modul enthält alles rund um Kompassfunktionen
 
 import mdhmc5883l
+import mdmpu6050
 
 # Diese Funktion holt die Werte aus dem Kompass- und Neigungssensor und
 # berechnet daraus den Winkel, den das Schiff zu Norden hat (Uhrzeigersinn)
@@ -11,4 +12,8 @@ def BoatToNorth():
     z = mdhmc588l.read_raw_data(0x05)
     y = mdhmc588l.read_raw_data(0x07)
     
+    # Neigungswinkel einlesen
+    tilt = mdmpu6050.GetTilt() # tilt wird zur Listenvariable: 0 = X, 1 = Y
+    print(tilt)
     
+BoatToNorth()
