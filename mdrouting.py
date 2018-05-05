8fc8a9f0487d0e75e5e8b7835dc290a39bc4097a
svensutter <32609779+svensutter@users.noreply.github.com>
2018-05-05 12:41:39 +0200

    Create mdrouting.py

---
+++ mdrouting.py
@@ -0,0 +1,2 @@
+# Das Modul mdrouting beinhaltet Funktionen zur Berechnung der Route/Navigation. Das heisst, hier werden
+# die Seekarten verarbeitet und anhand der aktuellen Position des Schiffes mögliche Wege generiert.
